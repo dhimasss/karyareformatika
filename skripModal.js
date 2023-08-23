f9ada21821c71041995cc400b158016b93939443
dhimasss <122718816+dhimasss@users.noreply.github.com>
2023-08-23 17:39:09 +0800

    Initial commit

---
+++ skripModal.js
@@ -0,0 +1,416 @@
+// {
+//   title: "mahadAlyTafsir", judul h1
+//   picture: img/gedungCsaintek  ambil gambar
+//   desc: "mahadAlyTafsir",     deskripsi p
+//   rectId:"mahadAlyTafsir"     id kotak yang sesuai dengan svg 
+  
+// }
+
+let dataGedung = [
+    {
+      title: "Gedung E Fakultas Saintek",
+      picture: "gambar.jpeg",
+      desc: "Gedung E Jurusan dari Fakultas Saintek",
+      rectId:"gedungESaintek"
+    },
+    {
+      title: "Gedung D Fakultas Saintek",
+      picture: "gambar2.jpg",
+      desc: "Gedung D Jurusan dari Fakultas Saintek",
+      rectId:"gedungDSaintek"
+    },
+    {
+      title: "Gedung F Fakultas Saintek",
+      picture: "gambar3.jpg",
+      desc: "Gedung F Jurusan dari Fakultas Saintek",
+      rectId:"gedungFSaintek"
+    },
+    {
+      title: "Gedung C Jurusan dari Fakultas Saintek",
+      picture: "gambar4.jpg",
+      desc: "Gedung C Jurusan dari Fakultas Saintek",
+      rectId:"gedungCSaintek"
+    } ]
+  //   {
+  //     title: "Masjid Agung Universitas Islam Negeri Alauddin",
+  //     desc: "Masjid Yang Terbesar di UIN Alauddin, Letaknya di dekat Pintu Masuk I",
+  //     rectId:"masjidAgung"
+  //   },
+  //   {
+  //     title: "Rektorat",
+  //     desc: "rektorat",
+  //     rectId:"rektorat"
+  //   },
+  //   {
+  //     title: "Gedung Dosen Saintek",
+  //     desc: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non ipsum accusamus facere consequuntur incidunt, eius harum saepe nemo inventore eum? ",
+  //     rectId:"gedungDosenSaintek"
+  //   },
+  //   {
+  //     title: "sekretHimpunanSaintek",
+  //     desc: "sekretHimpunanSaintek",
+  //     rectId:"sekretHimpunanSaintek"
+  //   },
+  //   {
+  //     title: "kantinRusunawa",
+  //     desc: "kantinRusunawa",
+  //     rectId:"kantinRusunawa"
+  //   },
+  //   {
+  //     title: "asramaRusunawa",
+  //     desc: "asramaRusunawa",
+  //     rectId:"asramaRusunawa"
+  //   },
+  //   {
+  //     title: "asramaPiba",
+  //     desc: "asramaPiba",
+  //     rectId:"asramaPiba"
+  //   },
+  //   {
+  //     title: "pascasarjanaC",
+  //     desc: "pascasarjanaC",
+  //     rectId:"pascasarjanaC"
+  //   },
+  //   {
+  //     title: "pascasarjanaB",
+  //     desc: "pascasarjanaB",
+  //     rectId:"pascasarjanaB"
+  //   },
+  //   {
+  //     title: "greenHouse",
+  //     desc: "greenHouse",
+  //     rectId:"greenHouse"
+  //   },
+  //   {
+  //     title: "pasacsarjanaA",
+  //     desc: "pasacsarjanaA",
+  //     rectId:"pasacsarjanaA"
+  //   },
+  //   {
+  //     title: "gedungR",
+  //     desc: "gedungR",
+  //     rectId:"gedungR"
+  //   },
+  //   {
+  //     title: "gedungFTK",
+  //     desc: "gedungFTK",
+  //     rectId:"gedungFTK"
+  //   },
+  //   {
+  //     title: "gedungDosen",
+  //     desc: "gedungDosen",
+  //     rectId:"gedungDosen"
+  //   },
+  //   {
+  //     title: "gedungFAH",
+  //     desc: "gedungFAH",
+  //     rectId:"gedungFAH"
+  //   },
+  //   {
+  //     title: "gedungFDK",
+  //     desc: "gedungFDK",
+  //     rectId:"gedungFDK"
+  //   },
+  //   {
+  //     title: "perpustakaan",
+  //     desc: "perpustakaan",
+  //     rectId:"perpustakaan"
+  //   },
+  //   {
+  //     title: "cafetaria",
+  //     desc: "cafetaria",
+  //     rectId:"cafetaria"
+  //   },
+  //   {
+  //     title: "gedungPKM",
+  //     desc: "gedungPKM",
+  //     rectId:"gedungPKM"
+  //   },
+  //   {
+  //     title: "masjidTarbiyah",
+  //     desc: "masjidTarbiyah",
+  //     rectId:"masjidTarbiyah"
+  //   },
+  //   {
+  //     title: "cafetariaSyariah",
+  //     desc: "cafetariaSyariah",
+  //     rectId:"cafetariaSyariah"
+  //   },
+  //   {
+  //     title: "asramaMahad",
+  //     desc: "asramaMahad",
+  //     rectId:"asramaMahad"
+  //   },
+  //   {
+  //     title: "gedungFSH",
+  //     desc: "gedungFSH",
+  //     rectId:"gedungFSH"
+  //   },
+  //   {
+  //     title: "gedungLPM",
+  //     desc: "gedungLPM",
+  //     rectId:"gedungLPM"
+  //   },
+  //   {
+  //     title: "poliklinik",
+  //     desc: "poliklinik",
+  //     rectId:"poliklinik"
+  //   },
+  //   {
+  //     title: "gedungBaruFKIK",
+  //     desc: "gedungBaruFKIK",
+  //     rectId:"gedungBaruFKIK"
+  //   },
+  //   {
+  //     title: "koperasi",
+  //     desc: "koperasi",
+  //     rectId:"koperasi"
+  //   },
+  //   {
+  //     title: "mahad",
+  //     desc: "mahad",
+  //     rectId:"mahad"
+  //   },
+  //   {
+  //     title: "BNI",
+  //     desc: "BNI",
+  //     rectId:"BNI"
+  //   },
+  //   {
+  //     title: "gedungFEBI",
+  //     desc: "gedungFEBI",
+  //     rectId:"gedungFEBI"
+  //   },
+  //   {
+  //     title: "aulaFEBI",
+  //     desc: "aulaFEBI",
+  //     rectId:"aulaFEBI"
+  //   },
+  //   {
+  //     title: "miniaturKabah",
+  //     desc: "miniaturKabah",
+  //     rectId:"miniaturKabah"
+  //   },
+  //   {
+  //     title: "lapangansepakbola",
+  //     desc: "lapangansepakbola",
+  //     rectId:"lapangansepakbola"
+  //   },
+  //   {
+  //     title: "tamanDarmaWanita",
+  //     desc: "tamanDarmaWanita",
+  //     rectId:"tamanDarmaWanita"
+  //   },
+  //   {
+  //     title: "lapanganVolley",
+  //     desc: "lapanganVolley",
+  //     rectId:"lapanganVolley"
+  //   },
+  //   {
+  //     title: "lapanganBasket",
+  //     desc: "lapanganBasket",
+  //     rectId:"lapanganBasket"
+  //   },
+  //   {
+  //     title: "gedungBaru2FKIK",
+  //     desc: "gedungBaru2FKIK",
+  //     rectId:"gedungBaru2FKIK"
+  //   },
+  //   {
+  //     title: "gedungLamaFKIK",
+  //     desc: "gedungLamaFKIK",
+  //     rectId:"gedungLamaFKIK"
+  //   },
+  //   {
+  //     title: "danauUIN",
+  //     desc: "danauUIN",
+  //     rectId:"danauUIN"
+  //   },
+  //   {
+  //     title: "Fakultas Usluhuddin dan Filsafat",
+  //     desc: " Fakultas Usluhuddin dan Filsafat",
+  //     rectId:"gedungFUF"
+  //   },
+  //   {
+  //     title: "Gedung Learn Teater",
+  //     desc: " Gedung Learn Teater",
+  //     rectId:"gedungTeater"
+  //   },
+  //   {
+  //     title: "P2B",
+  //     desc: "P2B",
+  //     rectId:"P2B"
+  //   },
+  //   {
+  //     title: "auditorium",
+  //     desc: "auditorium",
+  //     rectId:"auditorium"
+  //   },
+  //   {
+  //     title: "LP2M",
+  //     desc: "LP2M",
+  //     rectId:"LP2M"
+  //   },
+  //   {
+  //     title: "gedungPascasarjana2",
+  //     desc: "gedungPascasarjana2",
+  //     rectId:"gedungPascasarjana2"
+  //   },
+  //   {
+  //     title: "posSatpam",
+  //     desc: "posSatpam",
+  //     rectId:"posSatpam"
+  //   },
+  //   {
+  //     title: "lapanganUpacara",
+  //     desc: "lapanganUpacara",
+  //     rectId:"lapanganUpacara"
+  //   },
+  //   {
+  //     title: "mushallaFKIK",
+  //     desc: "mushallaFKIK",
+  //     rectId:"mushallaFKIK"
+  //   },
+  //   {
+  //     title: "mahadAlyTafsir",
+  //     desc: "mahadAlyTafsir",
+  //     rectId:"mahadAlyTafsir"
+  //   }
+  // ] 
+  
+/*
+  {
+    title: "mahadAlyTafsir", judul h1
+    picture: img/gedungCsaintek  ambil gambar
+    desc: "mahadAlyTafsir",     deskripsi p
+    rectId:"mahadAlyTafsir"     id kotak yang sesuai dengan svg 
+    
+  }
+  
+  */
+  
+  // // deklarasi variabel
+  // const modal = document.querySelector("[data-modal]")
+  // const openButton = document.querySelector("[data-open-modal]")
+  // const closeButton = document.querySelector("[data-close-modal]")
+  
+  
+  //============================================================
+  //untuk banyak
+  const modal = document.querySelector("[data-modal]")
+  const openButton = document.querySelector("[data-open-modal]")  //UNTUK TOMBOL OPEN
+  // const openButton = document.querySelectorAll(".kotak")
+  const closeButton = document.querySelector("[data-close-modal]")
+  //======================================================================
+  
+  
+  // kondisi ketika openButton ditekan
+  // openButton.addEventListener("click", () => {
+  //     modal.showModal()//menunjukkan posisi modal relative
+  //     console.log("ok")
+  //     // modal.showmodal() --> menunjukkan modal position fixed
+  // })
+  
+  
+  
+  //MEMBUKA MODAL KETIKA RECT DI TEKAN
+  // Menangkap elemen semua rect
+  
+  const rect = document.querySelectorAll('rect')
+    
+  // Menambahkan event listener pada tag rect
+  rect.forEach(tag => {
+    tag.addEventListener('click', function() {
+      // console.log(this.id)
+      //                 nama array.find ( variabel => item. NamaProperty === id dari yang di klik)
+      const idObjek = dataGedung.find(item => item.rectId === this.id)
+      // console.log(idObjek)
+        // tangkap id dari tag p yang id nya adalah deskripsi
+      const desc = document.getElementById("deskripsi")
+      //mengubah desc
+      desc.innerText = idObjek.desc
+        
+
+        //mengubah judul
+      const judul = document.getElementById("title")
+      judul.innerText = idObjek.title
+
+      // mengubah gambar
+      const pic = document.getElementById("picture")
+      pic.src = idObjek.picture
+
+      //menunjukkan modal yang isi nya telah diubah
+      modal.showModal()//ketika dipencet tag rect, akan muncul modal
+    });
+  });
+  
+  // untuk miniatur ka'bah (KARENA BULAT SENDIRI)
+  const ellipse = document.querySelectorAll('ellipse')
+  
+  ellipse.forEach(tagE => {
+    tagE.addEventListener('click', function() {
+      // console.log(this.id)
+      //                 nama array.find ( variabel => item. NamaProperty === id dari yang di klik)
+      const idObjek = dataGedung.find(item => item.rectId === this.id)
+      // console.log(idObjek)
+      //mengubah desc ellipse kabah
+      
+      //menunjukkan modal yang isi nya telah diubah
+      modal.showModal()//ketika dipencet tag rect, akan muncul modal
+    });
+  });
+  
+  
+  // buat objek {
+    // cari value dri properti objek
+    // cari value nya dengan array
+  
+  //   title
+  //   deskripsi
+  //   id
+  
+  
+  
+  // }
+  // cari id 
+  // cek id dengan isi 
+  
+  //memunculkan id rect
+  // rect.forEach(tag => {
+  //   tag.addEventListener('click', function() {
+  //     console.log(this.id)//ketika dipencet, akan memunculkan id dari rect
+  //   });
+  // });
+  
+  
+  //tutup modal
+  closeButton.addEventListener("click", () => {
+        modal.close()//tutup modal
+    //     
+    })
+   
+  
+  
+  
+  //tutup dialog diluar modal
+  modal.addEventListener("click", e => {
+      const dialogDimensions = modal.getBoundingClientRect()
+      if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+      ) {
+          modal.close()
+      }
+    })
+  
+  
+  
+  //CARA KUNO
+  //BUAT FUNGSI MENANGKAP KLIK ( ADDEVENTLISTENER)
+  // LALU TANGKAP ID DARI ELEMEN YANG DI KLIK (ID RECT, GETELEMENTBYID)
+  //SHOW MODAL DENGAN TANGKAP ELEMEN (TAGNAME)
+  // ..HAPPY CODING
+  
+  
